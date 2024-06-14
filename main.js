35cc457f508f108088032116d263840053f9dfb6
Arthur <110104912+Chevnncs@users.noreply.github.com>
2024-06-14 09:47:26 -0300

    14/06/24

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abas-conteudo");
 const tempo = document.querySelectorAll(".contador");
-
+tempo[0].textContent="ola"
+const tempoObjetivo1= new Date(2025,11,25,23,59,59);
+const tempoObjetivo2= new Date(2025,11,26,23,59,59);
+const tempoObjetivo3= new Date(2025,11,27,23,59,59);
+const tempoObjetivo4= new Date(2025,11,28,23,59,59);
+const agora= new Date();
+tempo[1].textContent=agora;
 console.log(tempo);
 console.log(botoes);
 for(let i=0; i <botoes.length; i++){
