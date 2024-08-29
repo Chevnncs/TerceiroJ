9e610be5d76c295a82dce53beed2127cc2a7ace2
Arthur <110104912+Chevnncs@users.noreply.github.com>
2024-08-29 08:11:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abas-conteudo");
 const tempo = document.querySelectorAll(".contador");
-const tempoObjetivo1= new Date(2023,11,25,23,59,59);
-const tempoObjetivo2= new Date(2023,11,26,23,59,59);
-const tempoObjetivo3= new Date(2023,11,27,23,59,59);
-const tempoObjetivo4= new Date(2023,11,28,23,59,59);
+const tempoObjetivo1= new Date(2024,11,25,23,59,59);
+const tempoObjetivo2= new Date(2024,11,26,23,59,59);
+const tempoObjetivo3= new Date(2024,11,27,23,59,59);
+const tempoObjetivo4= new Date(2024,11,28,23,59,59);
 function calculaTempo(tempoObjetivo){
 const agora= new Date();
 let segundos;
-let minitos
+let minutos
 let horas;
 let dias;
 
@@ -20,13 +20,18 @@ dias = Math.floor(horas/24)
 segundos = segundos%60
 horas = horas%24
 minutos = minutos%60
-dias = dias%24 
 if(segundos >= 0){
-return `faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos` 
+return [dias,horas,minutos,segundos] 
 } else {
     return 'prazo finalizado'
 }
 }
+const tempoUm = calculaTempo(tempoObjetivo1)
+const tempoDois = calculaTempo(tempoObjetivo2)
+const tempoTres = calculaTempo(tempoObjetivo3)
+const tempoQuatro = calculaTempo(tempoObjetivo4)
+
+
 tempo[0].textContent = calculaTempo(tempoObjetivo1)
 tempo[1].textContent = calculaTempo(tempoObjetivo2)
 tempo[2].textContent = calculaTempo(tempoObjetivo3)
