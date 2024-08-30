4a86d8990b97f7fb8d31eb57597d2dcb984105e6
Arthur <110104912+Chevnncs@users.noreply.github.com>
2024-08-30 09:40:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,57 @@
 const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".abas-conteudo");
-const tempo = document.querySelectorAll(".contador");
-const tempoObjetivo1= new Date(2024,11,25,23,59,59);
-const tempoObjetivo2= new Date(2024,11,26,23,59,59);
-const tempoObjetivo3= new Date(2024,11,27,23,59,59);
-const tempoObjetivo4= new Date(2024,11,28,23,59,59);
-function calculaTempo(tempoObjetivo){
-const agora= new Date();
-let segundos;
-let minutos
-let horas;
-let dias;
+const textos = document.querySelectorAll(".aba-conteudo");
 
-segundos = Math.floor((tempoObjetivo - agora)/1000)
-minutos = Math.floor(segundos/60)
-horas = Math.floor(minutos/60)
-dias = Math.floor(horas/24)
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-segundos = segundos%60
-horas = horas%24
-minutos = minutos%60
-if(segundos >= 0){
-return [dias,horas,minutos,segundos] 
-} else {
-    return 'prazo finalizado'
-}
-}
-const tempoUm = calculaTempo(tempoObjetivo1)
-const tempoDois = calculaTempo(tempoObjetivo2)
-const tempoTres = calculaTempo(tempoObjetivo3)
-const tempoQuatro = calculaTempo(tempoObjetivo4)
-
-
-tempo[0].textContent = calculaTempo(tempoObjetivo1)
-tempo[1].textContent = calculaTempo(tempoObjetivo2)
-tempo[2].textContent = calculaTempo(tempoObjetivo3)
-tempo[3].textContent = calculaTempo(tempoObjetivo4)
-
-for(let i = 0; i < botoes.length ; i++){
-    botoes[i].onclick = function(){
-        for (let j = 0 ; j < botoes.length ; j++){
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
-            conteudo[j].classList.remove("ativo")
+            textos[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo")
-        conteudo[i].classList.add("ativo")
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
+}
+
+function atualizaCronometro(){
+       
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);  
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+
+//comecaCronometro();
